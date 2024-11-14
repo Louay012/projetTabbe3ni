a3fd9694dac791805943719bc778af123efcc599
louay <louayhamdeni00@gmail.com>
2024-11-14 22:20:56 +0100

    +login + signup + routing +retouche css,...

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './home';
 import Login from './login';
+import Signup from './signup'
 import { createBrowserRouter , RouterProvider } from 'react-router-dom';
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
   {
   path:"/login",
     element:<Login/>
+  },
+  {
+  path:"/signup",
+    element:<Signup/>
   }
 ])
 function App() {
